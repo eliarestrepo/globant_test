7e2f6f8530dc99d9fbb9f170434a3ecd8706377d
Eliana Restrepo <erestrepo@tripadvisor.com>
2023-08-22 07:40:41 -0500

    endpoint to insert data from csv files

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,13 +1,42 @@
+from typing import List, Dict
+from app.database import insert_csv, insert_row, read_df, to_avro, insert_avro
+import json
 import os
+import sys
 from app import app
+from flask import request, render_template, send_file, make_response, url_for, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.figure import Figure
 
 
-
-
-
+@app.route("/historical")
+def historical() -> str:
+    """Inserts into the department, hired_employees and jobs tables the 
+    data found in the csv files in the files folder
+    Arguments: None
+    Returns: List with the names of the tables successfully inserted"""
+    path_root = os.getcwd()
+    tables_path = {
+        "hired_employees":"/app/files/hired_employees.csv",
+        "departments":"/app/files/departments.csv",
+        "jobs":"/app/files/jobs.csv"
+    }
+    success_list = []
+    failed_list = []
+    for table_name, path in tables_path.items():
+        try:         
+            insert_csv(table_name=table_name, file_path= f'{path_root}{path}')
+            success_list.append(table_name)
+        except Exception as e:
+            failed_list.append({table_name:e.args})
+            print(f"The historical insert in the {table_name} table failed {e.args}")
     
-
-
+    return json.dumps({'success': success_list, 'failed': failed_list })
    
 @app.route("/")
 def index():
@@ -19,7 +48,6 @@ def index():
         return f"Hello from {app_name} running in a Docker container behind Nginx!"
 
     return "Hello from Flask"
-
 
 
 
